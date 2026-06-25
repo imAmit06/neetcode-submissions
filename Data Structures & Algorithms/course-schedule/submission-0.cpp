9c6d41e6f42d7298cf1a5b859904b70bf54ba273
Amit Patil <74283636+imAmit06@users.noreply.github.com>
2026-06-25 12:27:13 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    bool isCycle(int src, vector<bool> &vis, vector<bool> &recPath, vector<vector<int>> graph) {
+        vis[src] = true;
+        recPath[src] = true;
+
+        for(int i=0; i<graph.size(); i++) {
+            int u = graph[i][1];
+            int v = graph[i][0];
+
+            if(u == src) {
+                if(!vis[v]) {
+                    if (isCycle(v, vis, recPath, graph)) {
+                        return true;
+                    }
+                } else {
+                    if(recPath[v]) {
+                        return true;
+                    }
+                }
+            }
+        }
+        recPath[src] = false;
+        return false;
+    }
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<bool> vis(numCourses, false);
+        vector<bool> recPath(numCourses, false);
+
+        for(int i=0; i<numCourses; i++) {
+            if(!vis[i]) {
+                if(isCycle(i, vis, recPath, prerequisites)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
