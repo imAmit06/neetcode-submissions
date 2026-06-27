e711519921b3469b3c8e9b2c1b5ce5fe7e80baf9
Amit Patil <74283636+imAmit06@users.noreply.github.com>
2026-06-27 10:27:47 +0530

    Add: cheapest-flight-path - submission-1

---
+++ Data Structures & Algorithms/cheapest-flight-path/submission-1.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<int> dist(n, 1e9);
+        dist[src] = 0;
+
+        for(int i=0; i<=k; i++) {
+            vector<int> temp = dist;
+
+            for(auto& flight : flights) {
+                int u = flight[0];
+                int v = flight[1];
+                int wt = flight[2];
+
+                if(dist[u] != 1e9 && dist[u] + wt < temp[v]) {
+                    temp[v] = dist[u] + wt;
+
+                }
+            }
+
+            dist = temp;
+        }
+
+        return dist[dst] == 1e9 ? -1 : dist[dst];
+    }
+};
